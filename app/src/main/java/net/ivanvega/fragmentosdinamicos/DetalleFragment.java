4646e6d22017021455e89199f35b3728e5b5e079
MikeMAMR <60702547+MikeMAMR@users.noreply.github.com>
2022-01-29 11:54:57 -0600

    arg_servicio

--- app/src/main/java/net/ivanvega/fragmentosdinamicos/DetalleFragment.java
+++ app/src/main/java/net/ivanvega/fragmentosdinamicos/DetalleFragment.java
@@ -38,6 +38,8 @@ public class DetalleFragment extends Fragment
 
 {
 
+
+    public static final String ARG_SERVICIO =  "idLibro";
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
