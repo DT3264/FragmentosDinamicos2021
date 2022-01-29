d95eaab785b02cd83263cebc05ee8d4b5fdd2409
DT3264 <cernadaniel32@gmail.com>
2022-01-29 10:21:32 -0600

    Actualizado servicio

--- app/src/main/java/net/ivanvega/fragmentosdinamicos/ServicioVinculado.java
+++ app/src/main/java/net/ivanvega/fragmentosdinamicos/ServicioVinculado.java
@@ -1,10 +1,15 @@
 package net.ivanvega.fragmentosdinamicos;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.MediaController;
@@ -16,6 +21,9 @@ import java.io.IOException;
 public class ServicioVinculado extends Service implements MediaController.MediaPlayerControl {
     String TAG = "ServicioVinculado";
     MediaPlayer mediaPlayer;
+
+    Libro libro;
+    int posLibro=0;
 
     @Override
     public void onCreate() {
@@ -36,7 +44,9 @@ public class ServicioVinculado extends Service implements MediaController.MediaP
         return mBinder;
     }
 
-    void prepareMediaPlayer(MediaPlayer.OnPreparedListener onPreparedListener, Uri uri) {
+    void prepareMediaPlayer(MediaPlayer.OnPreparedListener onPreparedListener, Libro libro) {
+        this.libro = libro;
+        Uri uri = Uri.parse(libro.getUrl());
         Log.d(TAG, "Preparando: " + uri.toString());
         if(mediaPlayer != null){
             mediaPlayer.reset();
@@ -45,18 +55,86 @@ public class ServicioVinculado extends Service implements MediaController.MediaP
         mediaPlayer = new MediaPlayer();
         try {
             mediaPlayer.setOnPreparedListener(onPreparedListener);
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+//                    ServicioVinculado.this.
+                    Log.d(TAG, "Deteniendo serviucio");
+                    ServicioVinculado.this.stopService(new Intent(ServicioVinculado.this, ServicioVinculado.class));
+                }
+            });
             mediaPlayer.setDataSource(getBaseContext(), uri);
             mediaPlayer.prepareAsync();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        createNotificationChannel();
+        setAsForeground();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+
+        String CHANNEL_ID = "1000";
+        String name = "Canal de audio";
+        String description = "Notificación que muestra la canción actual";
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel = new NotificationChannel(CHANNEL_ID, name, importance);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            channel.setDescription(description);
+        }
+        // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+        NotificationManager notificationManager = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            notificationManager = getSystemService(NotificationManager.class);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+    private void setAsForeground() {
+        for(int i=0; i<Libro.ejemplosLibros().size(); i++){
+            if(libro.getTitulo().equals(Libro.ejemplosLibros().elementAt(i).getTitulo())){
+                Log.d("Servicio", "Pos: " + i);
+                posLibro = i;
+            }
+        }
+
+        Log.d("Servicio", "Pos: " + posLibro);
+        String CHANNEL_ID = "1000";
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        notificationIntent.putExtra("flag_servicio", true);
+        notificationIntent.putExtra("pos", Integer.toString(posLibro));
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 2000, notificationIntent, PendingIntent.FLAG_MUTABLE);
+        Notification notification = null;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            notification = new Notification.Builder(this, CHANNEL_ID)
+                    .setContentTitle(libro.getTitulo())
+                    .setContentText(libro.getAutor())
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentIntent(pendingIntent)
+                    .setTicker("Se inicio el servicio")
+                    .build();
+        }
+        // Notification ID cannot be 0.
+        startForeground(2000, notification);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d(TAG, "Apagando serviucio");
         mediaPlayer.stop();
         mediaPlayer.release();
+        stopForeground(true);
     }
 
     @Override
